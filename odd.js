23e2aaabcc216cfb41972f689ab348b4c72f0c75
Saam Koukpari <saam.koukpari@gmail.com>
2021-08-30 01:31:13 +0000

    isOdd Complete

--- odd.js
+++ odd.js
@@ -1,7 +1,9 @@
-function isOdd(value) {
-	if (value % 2 === 0)
+function isOdd(num) {
+	if (num % 2 === 0) {
 		return false;
-	else
-		return true;
+  } else { 
+    return true;
+  }
 }
-console.log(value=3)
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));

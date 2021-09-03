c31c6553d36ad229aea2f04bb4730a4803533b8a
Saam Koukpari <saam.koukpari@gmail.com>
2021-09-03 10:26:18 -0700

    ingredients

---
+++ ingredients.js
@@ -0,0 +1,18 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+console.log("ingredients:");
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+ console.log("ingredients:");
+ for (let i = 0; i < ingredients.length; i++) {
+   console.log(ingredients[i]);
+ }
+
+ console.log("ingredients:");
+ for (let i = 7; i < ingredients.length; i++) {
+   console.log(ingredients[i]);
+ }
